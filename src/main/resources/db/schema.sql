267d3c3f2a104ede13d7ec70f2ec445dfd8a6123
jbpark <qufkr1029@gmail.com>
2025-12-01 07:00:33 +0900

    - user 테이블 문자열 비교 방법 변경

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -13,3 +13,18 @@ CREATE TABLE user (
     UNIQUE KEY uq_email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `role` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `note` text COLLATE utf8mb4_bin NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_id` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_id` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_user_id` (`user_id`),
+  UNIQUE KEY `uq_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
